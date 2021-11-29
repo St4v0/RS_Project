17cc323c0940a49432e463f845939e21a48ab38e
Alele Famous <famousalele@gmail.com>
2021-11-29 21:12:44 +0000

    Implement band width filter for e_signal

--- Light_Follower.ino
+++ Light_Follower.ino
@@ -3,19 +3,19 @@
 #include "encoders.h"
 #include "pid.h"
 
-#define GAIN_CONST 200
+#define GAIN_CONST 1000
 #define READ_INTERVAL 50
-#define FWD  0
+#define FWD  30
 
 unsigned long current_ts;
 unsigned long bump_ts = 0;
 unsigned long elapsed_t;
-float lpwm = 0;
-float rpwm = 0;
 float error_line = 0;
 BumpSensor_c bumpSensors;
 Motors_c motors;
 PID_c pid;
+float lpwm = 0;
+float rpwm = 0;
 
 void calibration(){
   motors.fullRotate();
@@ -32,28 +32,26 @@ void setup() {
   setupEncoder0();
   setupEncoder1();
   delay(3000);
-  bumpSensors.Calibrate_Bump_Sensors();
+//  bumpSensors.Calibrate_Bump_Sensors();
+calibration();
   bumpSensors.Display_Lapsed_Calibration();
   motors.setMotorsPwm(FWD, FWD);
 }
 
 void loop() {
-  // put your main code here, to run  repeatedly:
+  // put your main code here, to run repeatedly:
   current_ts = millis();
-  motors.setMotorsPwm(FWD, FWD);
+//  motors.setMotorsPwm(FWD, FWD);
   elapsed_t = current_ts - bump_ts;
   if(elapsed_t >= READ_INTERVAL){
     bumpSensors.Read_Bump_Sensors();
-    bumpSensors.Display_Lapsed_Time();
-    
-    error_line = bumpSensors.getErrorLine();
-    error_line *= GAIN_CONST;
-        /*Serial.print("The ORIGINl Error Light is: ");
-    Serial.println(error_line, 5);
-    error_line *= GAIN_CONST;
+//    Serial.print("The ORIGINl Error Light is: ");
+//    Serial.println(error_line, 5);
+    error_line = GAIN_CONST * bumpSensors.getErrorLine();
+    if(error_line > 100 || error_line < -80)error_line = 0;
     Serial.print("The Error Light is: ");
     Serial.println(error_line);
-    if(error_line < -1){
+    /*if(error_line < -1){
       motors.rotate(LOW);
       bumpSensors.Display_Lapsed_Time();
       Serial.println("Turn LEFT!!!");
@@ -68,22 +66,10 @@ void loop() {
     }*/
     lpwm = FWD + error_line;
     rpwm = FWD - error_line;
-    Serial.print(lpwm);
-    Serial.print(", ");
-    Serial.print(rpwm);
-    motors.setMotorsPwm(rpwm, lpwm);
-    if(lpwm < -1){
-      
-      Serial.println("Turn LEFT!!!");
-    }else if(rpwm > 3){
-      
-      Serial.println("Turn RIGHT!!!");
-    }else{
-      
-      Serial.println("STOP MOTION!!!");
-    }
-    
+    motors.setMotorsPwm(lpwm, rpwm);
     motors.powerBothMotors();
+    Serial.print("");
+    Serial.println();
     bump_ts = millis();
   }
 
